b686cd2a419bc344623928d73bcb847c11233e22
Larry Price <larry.price.dev@gmail.com>
2013-12-05 20:24:10 -0500

    url validation

--- wiki.go
+++ wiki.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+  "errors"
   "html/template"
   "io/ioutil"
   "net/http"
+  "regexp"
 )
 
 type Page struct {
@@ -32,8 +34,20 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
       http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -43,7 +57,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -52,10 +69,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
+    err = p.save()
     if err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
@@ -64,6 +84,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
